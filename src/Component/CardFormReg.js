9548792d469db021cf31aba7b150bbd83da79a82
naufal55 <moehhammadnofal@gmail.com>
2022-04-17 12:12:30 +0700

    penambahan fitur register

--- src/Component/CardFormReg.js
+++ src/Component/CardFormReg.js
@@ -1,9 +1,82 @@
 import React, { useState } from "react";
+import { Card, Col, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import InputColumn from "../Atom/InputColumn";
+import MainButton from "../Atom/MainButton";
 
 const CardFormReg = (props) => {
- return(
-   <></>
-  )
+  const navigate = useNavigate();
+
+  const [state, setState] = useState({
+    namaLengkap: "",
+    email: "",
+    password: "",
+  });
+
+  const handleOnChange = (e) => {
+    //update nilai saat isi value di kolom input
+    // setState here
+    setState({
+      ...state, //extarct state
+      //beri nilai setState untuk value yg di kolom input, sesuai dengna name form control
+      [e.target.name]: e.target.value,
+    });
+    console.log(state);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault(); //mencegah reload
+    //print state value with console.log here
+    console.log(state); //tampilkan yang diketik disini
+
+    // const user = JSON.stringify(state)
+    // localStorage.setItem('user', user)
+    // const data = localStorage.getItem('user')
+    // const dataUser =  JSON.parse(data)
+    // console.log(dataUser);
+  };
+  const handleRegister = () => {
+    navigate("/login");
+  };
+
+  return (
+    <>
+      <Col md={4} sm={12} className="bg-dark rounded-3 h-100 pb-2">
+        <Card.Body>
+          <h2 className="text-light my-3">{props.btnName}</h2>
+          <Form onSubmit={handleOnSubmit}>
+            <div className="my-4">
+              <InputColumn
+                change={handleOnChange}
+                value={state.namaLengkap}
+                name="namaLengkap"
+                type="text"
+                holder="Name"
+              />
+              <InputColumn
+                change={handleOnChange}
+                value={state.email}
+                name="email"
+                type="email"
+                holder="Email"
+              />
+              <InputColumn
+                change={handleOnChange}
+                value={state.password}
+                name="password"
+                type="password"
+                holder="Password"
+              />
+            </div>
+            <MainButton click={handleRegister} type="submit" btn={props.btnName} color="danger" />
+          </Form>
+        </Card.Body>
+      </Col>
+      {/* <p className="text-light">Nama : {state.namaLengkap}</p>
+      <p className="text-light">email : {state.email}</p>
+      <p className="text-light">pass : {state.password}</p> */}
+    </>
+  );
 };
 
 export default CardFormReg;
--- src/Pages/RegisterPages.js
+++ src/Pages/RegisterPages.js
@@ -1,11 +1,17 @@
 import React from "react";
-
+import { Container, Row } from "react-bootstrap";
+import BodyPages from "../Component/BodyPages";
+import CardFormReg from "../Component/CardFormReg";
 
 const RegisterPages = () => {
   return (
-    <>
-    </>
-    )
+    <Container className="d-grid min-vh-100">
+    <Row className="gap-4 justify-content-center align-items-center my-auto bg-none">
+        <BodyPages colorLog="danger" colorReg="none" disableReg="disabled" />
+        <CardFormReg btnName="Register" />
+      </Row>
+    </Container>
+  );
 };
 
 export default RegisterPages;
