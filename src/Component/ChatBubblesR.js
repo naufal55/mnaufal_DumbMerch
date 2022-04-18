db86b4d4c398fd77d9c50d3f3a4665deba8925b2
naufal55 <moehhammadnofal@gmail.com>
2022-04-18 12:56:24 +0700

    update fitur complain page

--- src/Component/ChatBubblesR.js
+++ src/Component/ChatBubblesR.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const ChatBubblesR = (props) => {
   return (
-    <div class="d-flex flex-shrink-0 me-auto pb-4">
+    <div class="d-flex flex-row-reverse flex-shrink-0 ms-0 pb-4">
       <div>
         <img
           src={props.src}
-          class="rounded-circle ms-2"
+          class="rounded-circle me-2"
           width="40"
           height="40"
         />
       </div>
-      <div class="flex-shrink-1 bg-secondary rounded py-2 px-3 ms-3">
+      <div class="flex-shrink-1 bg-dark rounded py-2 px-3 me-3">
         {props.messages}
       </div>
     </div>
--- src/Pages/ChatComplain.js
+++ src/Pages/ChatComplain.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
 import InputColumn from "../Atom/InputColumn";
-import MainButton from "../Atom/MainButton";
-import ChatBubblesL from "../Component/ChatBubblesL";
-import ChatBubblesR from "../Component/ChatBubblesR";
+import ChatBubblesL from "../Component/ChatBubblesR";
+import ChatBubblesR from "../Component/ChatBubblesL";
 import ChatList from "../Component/ChatList";
 
 const ChatComplain = () => {
   return (
-    <Container className="bg-dark" style={{height:"85vh"}}>
     
-      <Row className="px-2">
+    
+      <Row className="mx-5 bg-none" style={{height:"85vh"}}>
         <Col md={4} className="border border-bottom-0 border-top-0 border-start-0 border-white" style={{height:"85vh"}}>
           <Card className="my-2 h-auto bg-dark">
             <ChatList 
@@ -22,16 +21,14 @@ const ChatComplain = () => {
         <Col md={8} className="border border-0">
           <div className="position-relative overflow-auto" style={{height:"70vh"}}>
               <div className="d-flex flex-column mt-3 text-light">
-
-                <ChatBubblesR
-                  src="https://bootdey.com/img/Content/avatar/avatar5.png" 
-                  messages="yes, is there anything can i help ?"
-                /> 
                 <ChatBubblesL
                   src="https://bootdey.com/img/Content/avatar/avatar1.png" 
                   messages="Hello admin, I need your help"
                 />
-
+                <ChatBubblesR
+                  src="https://bootdey.com/img/Content/avatar/avatar5.png" 
+                  messages="yes, is there anything can i help ?"
+                /> 
                </div>
           </div>
           <div class="flex-grow-0 py-3 px-4 border-top">
@@ -41,7 +38,7 @@ const ChatComplain = () => {
         
       </Row>
           
-    </Container>
+
   );
 };
 
